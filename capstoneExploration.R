fed682e94e08a48dfc037ca58661ce3b2672f94b
jmfuch02 <jason.fuchs@gmail.com>
2015-07-16 00:36:04 -0400

    Quiz 1 done
    Started work on sampling function

--- capstoneExploration.R
+++ capstoneExploration.R
@@ -1,10 +1,38 @@
-# Text files
-#~/Coursera-SwiftKey/final/en_US/en_US.blogs.txt
-#~/Coursera-SwiftKey/final/en_US/en_US.news.txt
-#~/Coursera-SwiftKey/final/en_US/en_US.twitter.txt
 
-con <- file("Coursera-SwiftKey/final/en_US/en_US.blogs.txt", "r")
-line <- readLines(con, 5)
-line <- readLines(con, 10)
-readLines(con, 1)
-close(con)
+# This function takes a file and samples some percentage of lines from it
+# It creates a new file with the sampled lines only
+
+sampleLines <- function(oldFileName, percentage, newFileName){
+
+    con <- file(oldFileName, "r")
+    if (file.exists(newFileName)) {
+        file.remove(newFileName)
+    }
+    
+    file.create(newFileName)
+    
+    #TODO: Fix this error on last line
+    while (nchar(line <- readLines(con, 1)) > 0) { 
+        
+        if (rbinom(1, 1, percentage) == 1) {
+            cat(line, file = newFileName, append = TRUE, sep = "\n")
+        }
+    }
+    
+    close(con)
+}
+
+# Now apply this function to the English database
+
+set.seed(12345)
+sampleLines(oldFileName = "Coursera-SwiftKey/final/en_US/en_US.blogs.txt",
+            percentage = 0.1,
+            newFileName = "sample_en_US.blogs.txt")
+
+sampleLines(oldFileName = "Coursera-SwiftKey/final/en_US/en_US.twitter.txt",
+            percentage = 0.1,
+            newFileName = "sample_en_US.twitter.txt")
+
+sampleLines(oldFileName = "Coursera-SwiftKey/final/en_US/en_US.news.txt",
+            percentage = 0.1,
+            newFileName = "sample_en_US.news.txt")
--- capstoneQuizzes.R
+++ capstoneQuizzes.R
@@ -18,3 +18,33 @@ close(con)
 
 
 # Q1.4: love and hate
+
+con <- file("Coursera-SwiftKey/final/en_US/en_US.twitter.txt", "r")
+
+loveLines <- 0
+hateLines <- 0
+
+while (nchar(line <- readLines(con, 1)) > 0) {
+    
+    if ((grepl("love", x = line)) == TRUE) {
+        loveLines <- loveLines + 1
+    }
+    
+    if ((grepl("hate", x = line)) == TRUE) {
+        hateLines <- hateLines + 1
+    }
+}
+
+loveLines / hateLines
+
+close(con)
+
+
+con <- file("Coursera-SwiftKey/final/en_US/en_US.twitter.txt", "r")
+line <- readLines(con, 1)
+grepl("aredfvb", x = line) == FALSE
+close(con)
+
+
+
+
