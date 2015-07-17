a809ce40f1454d15edab761457a6150d6a44ba8c
jmfuch02 <jason.fuchs@gmail.com>
2015-07-17 00:26:13 -0400

    Sampled texts

--- capstoneExploration.R
+++ capstoneExploration.R
@@ -1,18 +1,24 @@
+setwd("~/r_wdir/2015-07_capstone/Capstone")
+library(tm)
 
 # This function takes a file and samples some percentage of lines from it
 # It creates a new file with the sampled lines only
 
+
 sampleLines <- function(oldFileName, percentage, newFileName){
 
+    # Open a new connection to the file
     con <- file(oldFileName, "r")
+    
+    # Make sure the new file is blank
     if (file.exists(newFileName)) {
         file.remove(newFileName)
     }
     
     file.create(newFileName)
     
-    #TODO: Fix this error on last line
-    while (nchar(line <- readLines(con, 1)) > 0) { 
+    # Read each line and flip a coin to decide whether to include it
+    while (length(line <- readLines(con, 1)) > 0) { 
         
         if (rbinom(1, 1, percentage) == 1) {
             cat(line, file = newFileName, append = TRUE, sep = "\n")
@@ -36,3 +42,21 @@ sampleLines(oldFileName = "Coursera-SwiftKey/final/en_US/en_US.twitter.txt",
 sampleLines(oldFileName = "Coursera-SwiftKey/final/en_US/en_US.news.txt",
             percentage = 0.1,
             newFileName = "sample_en_US.news.txt")
+
+# Create the corpus
+dirName <- file.path(".", "samples")
+length(dir(dirName))
+docs <- Corpus(DirSource(dirName))
+summary(docs)
+str(docs[1])
+
+# Do some cleaning
+
+# Remove symbols
+# Convert to lowercase
+# Remove numbers
+# Remove punctuation
+# Remove stop words
+
+
+
