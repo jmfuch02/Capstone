3bb80ca828e4d69d126c390cd25b5ac0df43d6b8
jmfuch02 <jason.fuchs@gmail.com>
2015-07-21 00:02:27 -0400

    Stemmed words, cut sparsity, made wordclouds

--- capstoneTokenization.R
+++ capstoneTokenization.R
@@ -3,9 +3,9 @@ library(tm)
 
 # Create the corpus
 dirName <- file.path(".", "samples")
-length(dir(dirName))
+#length(dir(dirName))
 docs <- Corpus(DirSource(dirName))
-str(docs)
+#str(docs)
 
 # Do some cleaning
 
@@ -30,10 +30,46 @@ docs <- tm_map(docs, removeWords, stopwords("english"))
 # Remove extra white space
 docs <- tm_map(docs, stripWhitespace)
 
-# View the files now
-str(docs)
-
 # Stem words
+library(SnowballC)
+docs <- tm_map(docs, stemDocument)
 
+# View the files now
+docs[1][[1]][[1]][1]
+docs[2][[1]][[1]][1]
+docs[3][[1]][[1]][1]
 
+# Create the document term matrix
+dtm <- DocumentTermMatrix(docs)
 
+# Look at the first 10 words
+inspect(dtm[1:3, 1:10])
+
+# Look at 10 most frequent words
+freq <- colSums(as.matrix(dtm))
+ord <- order(freq)
+freq[head(ord, 10)]
+freq[tail(ord, 10)]
+
+# Look at frequencies of frequencies
+head(table(freq), 15)
+tail(table(freq), 15)
+
+dim(dtm)
+dtms <- removeSparseTerms(dtm, 0.05)
+dim(dtms)
+
+# Now check that there are fewer terms and less sparsity
+dtm
+dtms
+
+freq <- colSums(as.matrix(dtms))
+ord <- order(freq)
+freq[head(ord, 15)]
+freq[tail(ord, 15)]
+
+# Make a word cloud
+library(wordcloud)
+set.seed(123)
+wordcloud(names(freq), freq, min.freq = 10000,
+          colors = brewer.pal(6, "Dark2"), scale = c(3, 0.1))
