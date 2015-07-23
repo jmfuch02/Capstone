102b600426db1466f24246663c196c94c970956c
jmfuch02 <jason.fuchs@gmail.com>
2015-07-23 16:49:16 -0400

    Working on n-gram model

--- capstoneTokenization.R
+++ capstoneTokenization.R
@@ -3,7 +3,7 @@
 # Graham.Williams@togaware.com
 # 5th November 2014
 
-setwd("~/r_wdir/2015-07_capstone/Capstone")
+#setwd("~/r_wdir/2015-07_capstone/Capstone")
 library(tm)
 library(SnowballC)
 library(wordcloud)
@@ -17,9 +17,7 @@ library(RWeka)
 
 # Create the corpus
 dirName <- file.path(".", "samples")
-#length(dir(dirName))
 docs <- Corpus(DirSource(dirName))
-#str(docs)
 
 # Do some cleaning
 
@@ -33,9 +31,9 @@ docs <- Corpus(DirSource(dirName))
 docs <- tm_map(docs, content_transformer(tolower))          # Convert to lowercase
 docs <- tm_map(docs, removeNumbers)                         # Remove numbers
 docs <- tm_map(docs, removePunctuation)                     # Remove punctuation
-docs <- tm_map(docs, removeWords, stopwords("english"))     # Remove stop words
 docs <- tm_map(docs, stripWhitespace)                       # Remove extra white space
-docs <- tm_map(docs, stemDocument)                          # Stem words
+#docs <- tm_map(docs, removeWords, stopwords("english"))     # Remove stop words
+#docs <- tm_map(docs, stemDocument)                          # Stem words
 
 # View the files now
 docs[1][[1]][[1]][1]
@@ -45,10 +43,10 @@ docs[3][[1]][[1]][1]
 # Create the document term matrix
 dtm <- DocumentTermMatrix(docs)
 
-# Look at the first 10 words
-inspect(dtm[1:3, 1:10])
+# Remove sparse terms
+dtms <- removeSparseTerms(dtm, 0.05)
 
-# Look at 10 most frequent words
+# Look at 10 least and most frequent words
 freq <- colSums(as.matrix(dtm))
 ord <- order(freq)
 freq[head(ord, 10)]
@@ -58,41 +56,10 @@ freq[tail(ord, 10)]
 head(table(freq), 15)
 tail(table(freq), 15)
 
-dim(dtm)
-dtms <- removeSparseTerms(dtm, 0.05)
-dim(dtms)
-
-# Now check that there are fewer terms and less sparsity
-dtm
-dtms
-
-freq <- colSums(as.matrix(dtms))
-ord <- order(freq)
-freq[head(ord, 15)]
-freq[tail(ord, 15)]
-
 # Make a word cloud
 set.seed(123)
 wordcloud(names(freq), freq, min.freq = 10000,
           colors = brewer.pal(6, "Dark2"), scale = c(3, 0.1))
-
-# Do some quantitative analysis
-#words <- colnames(as.matrix(dtm))[nchar(colnames(as.matrix(dtm))) < 20]
-words <- dtm %>%
-    as.matrix %>%
-    colnames %>%
-    (function(x) x[nchar(x) < 20])
-
-summary(nchar(words))
-dist_tab(nchar(words))
-
-# Create a histogram of the word lengths
-data.frame(nletters=nchar(words)) %>%
-    ggplot(aes(x=nletters)) +
-    geom_histogram(binwidth=1) +
-    geom_vline(xintercept=mean(nchar(words)),
-               colour="green", size=1, alpha=.5) +
-    labs(x="Number of Letters", y="Number of Words")
 
 # Show most common letters
 words %>%
@@ -119,12 +86,29 @@ TrigramTokenizer <- function (x) {
     NGramTokenizer(x, Weka_control(min = 3, max = 3))
 }
 
+# Create 2-gram tdm and remove sparse terms
 tdm2 <- TermDocumentMatrix(docs, control = list(tokenize = BigramTokenizer))
-inspect(tdm2[1000000:1000020,])
+tdm2s <- removeSparseTerms(tdm2, 0.4)
 
+# Create 3-gram tdm and remove sparse terms
 tdm3 <- TermDocumentMatrix(docs, control = list(tokenize = TrigramTokenizer))
-inspect(tdm3[1000000:1000020,])
+tdm3s <- removeSparseTerms(tdm3, 0.4)
 
-findFreqTerms(tdm2, 5000)
-findFreqTerms(tdm3, 500)
+# Here are some of the most frequent 2-grams.
+term2Freq <- rowSums(as.matrix(tdm2s))
+ord2Terms <- order(term2Freq)
+term2Freq[tail(ord2Terms, 10)]
+term2Freq[head(ord2Terms, 10)]
+
+# Here are some of the most frequent 3-grams.
+term3Freq <- rowSums(as.matrix(tdm3s))
+ord3Terms <- order(term3Freq)
+term3Freq[tail(ord3Terms, 10)]
+term3Freq[head(ord3Terms, 10)]
+
+inspect(tdm3s["one of the",])
+term3Freq["one of the"]/length(term3Freq)
+
+
+
 
